02bc1c995789d38ea83845c6c1ba8d8ca3c80544
StillUsefull <109765950+StillUsefull@users.noreply.github.com>
2022-12-06 23:09:20 +0200

    add simple router for /dishes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+
 
 const hostName = 'localhost';
 const port = '3000';
@@ -11,34 +13,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
-
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    next();
-});
-
-app.get('/dishes', (req, res) => {
-    res.end('Will send all dishes to you');
-});
-
-app.get('/dishes/:dishId', (req, res) => {
-    console.log(req.params.dishId)
-    res.end(`We will send you details about dish: ${req.params.dishId}`);
-})
-
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.details);
-});
-   
-
-app.put('/dishes', (req, res) => {
-    res.end(`Dish with name: ${req.body.name} and details: ${req.body.details} was added to database`);
-});
-
-app.delete('/dishes', (req, res) => { 
-    res.end(`Dish with name: ${req.body.name} and details: ${req.body.details} was deleted from database`);
-});
+app.use('/dishes', dishRouter);
 app.listen(port, hostName, () => {
     console.log(`Server is started at http//${hostName}:${port}`);
 });
