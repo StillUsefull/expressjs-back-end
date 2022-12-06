a6d02add9433ad714967d78196f84fde282f36ee
StillUsefull <109765950+StillUsefull@users.noreply.github.com>
2022-12-07 00:02:35 +0200

    add leaderRouter and promoRouter

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const http = require('http');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
-const dishRouter = require('./routes/dishRouter');
 
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 
 const hostName = 'localhost';
 const port = '3000';
@@ -14,6 +16,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use('/dishes', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
 app.listen(port, hostName, () => {
     console.log(`Server is started at http//${hostName}:${port}`);
 });
