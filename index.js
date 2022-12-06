eed58f782ee8fa898bfc638c6b7124e16ac27e5d
StillUsefull <109765950+StillUsefull@users.noreply.github.com>
2022-12-06 10:51:27 +0200

    update 1 week

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+
 var rect = require('./rectangle');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
-
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(rectangle.perimeter(l,b), rectangle.area(l,b))
+        }
+    });
 }
 
 solveRect(2,4);
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,17 @@
-exports.perimeter =  (x, y) => (2*(x+y));
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() => {
+            callback(new Error('x or y is less than 0'), null)
+        }, 2000)
+    } 
+    else {
+        setTimeout(() => {
+            callback(null, 
+                {
+                    perimeter: (x, y) => (2*(x+y)),
+                    area: (x, y) => (x*y)
+                });
+        }, 2000)
+    }
+}
 
-exports.area = (x, y) => (x*y);
