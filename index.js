8b1dfb3ca1e4290619b6799edcba853fbe7c58c0
StillUsefull <109765950+StillUsefull@users.noreply.github.com>
2022-12-06 15:15:13 +0200

    Simple Express Example

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-const http = require("http");
-const fs = require('fs');
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
 const path = require('path');
 
-const basePublicPath = path.resolve(__dirname, 'public');
 
-const hostName = "localhost";
-const Port = 3000;
+const hostName = 'localhost';
+const port = '3000';
 
-const server = http.createServer((req, res) => {
-    let fileURL;
-    if (req.method == 'GET') {
-        
-        if (req.url == '/') {
-            fileURL = '/index.html';
-        } 
-        else if (req.url == '/about'){
-            fileURL = '/about.html';
-        }
-    }
-    else {
+const app = express();
+app.use(morgan('dev'));
 
-    }
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use((req, res, nest) => {
     res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    res.end(fs.readFileSync(basePublicPath+fileURL));
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
 });
 
-server.listen(Port, hostName, () => {
-    console.log(`Server is started at hostName: ${hostName} and port: ${Port}`);
-})
+app.listen(port, hostName, () => {
+    console.log(`Server is started at http//${hostName}:${port}`);
+});
