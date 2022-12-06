098d2c3fcfda34aee493887449d5623bf7d38082
StillUsefull <109765950+StillUsefull@users.noreply.github.com>
2022-12-06 14:36:28 +0200

    add base routing

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const http = require("http");
+const fs = require('fs');
+const path = require('path');
+
+const basePublicPath = path.resolve(__dirname, 'public');
 
 const hostName = "localhost";
 const Port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
+    let fileURL;
+    if (req.method == 'GET') {
+        
+        if (req.url == '/') {
+            fileURL = '/index.html';
+        } 
+        else if (req.url == '/about'){
+            fileURL = '/about.html';
+        }
+    }
+    else {
+
+    }
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+    res.end(fs.readFileSync(basePublicPath+fileURL));
 });
 
 server.listen(Port, hostName, () => {
