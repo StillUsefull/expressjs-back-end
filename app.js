d0affbbaf854b62f75b62d1111bba4e756b3f5ca
StillUsefull <109765950+StillUsefull@users.noreply.github.com>
2022-12-15 00:47:52 +0200

    add last assignment

--- app.js
+++ app.js
@@ -1,87 +1,107 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-const assert = require('assert');
-const dboper = require('./operations');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 var config = require('./config');
-const url = config.mongoUrl;
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const leaderRouter = require('./routes/leaderRouter');
-const dishRouter = require('./routes/dishRouter');
-const promoRouter = require('./routes/promoRouter');
-const uploadRouter = require('./routes/uploadRouter');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var leaderRouter = require('./routes/leaderRouter');
+var promoRouter = require('./routes/promoRouter');
+var uploadRouter = require('./routes/uploadRouter');
+var favouriteRouter = require('./routes/favouriteRouter');
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
 
-var app = express();
+const url = config.mongoUrl;
 
-
-
-const dbname = 'conFusion';
-
-
-const connect = mongoose.connect(path.resolve(url, dbname));
+const connect = mongoose.connect(url, {
+  useMongoClient: true
+});
 
 connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+  console.log('Connected to server!');
+}, (err) => {
+  console.log(err);
+});
 
+var app = express();
+
+app.all('*', (req, res, nxt) => {
+  if(req.secure) {
+    return nxt();
+  } else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(session({
-  name: 'session',
-  secret: '12345-67890-09876-54321',
+  name: 'session-id',
+  secret: 'Jesus-Loves-w@ch!ra-so-Much!!',
   saveUninitialized: false,
   resave: false,
   store: new FileStore()
 }));
-
-
-
-app.use(auth);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-        next();
-  }
-}
+// app.use(session({secret: 'SECRET'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use('/', index);
+app.use('/users', users);
+
+// authentication before resource access
+// function auth(req, res, next) {
+//   // console.log(req.signedCookies);
+//   console.log(req.session);
+
+//   // if(!req.signedCookies.user){
+//   if(!req.user){  
+//     // var authHeader = req.headers.authorization;
+//     // if(!authHeader){
+//       var err = new Error('You are not authenticated!');
+//       // res.setHeader('WWW-Authenticate', 'Basic');
+//       err.status = 403;
+//       return next(err);
+//     }
+//     else {
+//       // if(req.signedCookies.user === 'admin'){ 
+//       next();     
+//     }
+// }
+// app.use(auth);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
-app.use('/imageUpload',uploadRouter);
+app.use('/promotions', promoRouter);
+app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favouriteRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
